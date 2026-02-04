ba6cf552717aaf5c2c66fabe8e5e13f307cebff7
josuecarrera <josuecarrera05@outlook.com>
2026-02-04 00:04:39 +0000

    grafana actual

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hellooooo Worldd Devs
         </a>
       </header>
     </div>
